a9636013925584253c90c3e28fde551c83e5583a
ManyataJain <65446562+ManyataJain@users.noreply.github.com>
2020-07-30 16:53:50 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,27 @@
+class Form{
+constructor(){
+
+
+}
+display(){
+    var title=createElement('h2')
+    title.html("car racing game");
+    title.position(130,0);
+
+    var input=createInput("name");
+    var button=createButton('play');
+    input.position(130,160);
+    button.position(250,200);
+     var greeting=createElement('h3');
+    
+
+    button.mousePressed(function(){
+        input.hide();
+        button.hide();
+        var name=input.value();
+        greeting.html("hello" + name);
+        greeting.position(130,160);
+    })
+}
+
+}
